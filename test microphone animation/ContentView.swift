ed3c5d43bb515170c8824d11fe907f70deb4b051
Nicolas Bonnet <nicolas.bonnet@weforum.org>
2025-10-11 14:27:17 +0200

    Initial Commit

---
+++ test microphone animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test microphone animation
+//
+//  Created by Nicolas Bonnet on 11.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test microphone animation/test_microphone_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_microphone_animationApp.swift
+//  test microphone animation
+//
+//  Created by Nicolas Bonnet on 11.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct test_microphone_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
